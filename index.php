cd2d53bd9975c2cef20fc5701c047e5315381033
Herat Gandhi <herat_000@yahoo.co.in>
2012-06-19 10:41:25 +0530

    export from module

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require (AT_INCLUDE_PATH.'header.inc.php');
 <script language="javascript" type="text/javascript" src="<?php echo AT_BASE_HREF; ?>mods/calendar/fullcalendar/fullcalendar-theme.js"></script>
 <link href= "<?php echo AT_BASE_HREF; ?>mods/calendar/fullcalendar/fullcalendar-theme.css" rel="stylesheet" type="text/css"/> 
 <a href="mods/calendar/file_import.php"><?php echo _AT("at_cal_import_file"); ?></a>
+<a align="right" href="mods/calendar/test_export.php">Export ics file</a>
 <script>
     $(document).ready(function () {
         /* Get current date for calculations. */
--- test_export.php
+++ test_export.php
@@ -1,15 +1,53 @@
 <?php
+
+define('AT_INCLUDE_PATH', '../../include/');
+require (AT_INCLUDE_PATH.'vitals.inc.php');
+global $db;
+
 $ical = "BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//hacksw/handcal//NONSGML v1.0//EN";
 
-$db = mysql_connect("localhost","root","root");
-mysql_select_db("atutor",$db);
+//$db = mysql_connect("localhost","root","root");
+//mysql_select_db("atutor",$db);
 
-$qry = "SELECT * FROM at_full_calendar_events";
-$res = mysql_query($qry,$db);
+$qry = "SELECT * FROM `".TABLE_PREFIX."full_calendar_events` WHERE userid='".$_SESSION['member_id']."'";
+$result = mysql_query($qry,$db);
 
-while($row = mysql_fetch_array($res))
+$rows = array();
+
+while ($row = mysql_fetch_assoc($result)) 
+{
+    array_push( $rows, $row );        
+}
+
+global $moduleFactory;
+
+$coursesmod = $moduleFactory->getModule("_core/courses");
+$courses=$coursesmod->extend_date();    
+if( $courses != "" )
+{
+    foreach ( $courses as $event )
+        array_push( $rows, $event );
+}
+
+$assignmentsmod = $moduleFactory->getModule("_standard/assignments");
+$assignments=$assignmentsmod->extend_date();
+if( $assignments != "" )
+{
+    foreach ( $assignments as $event )
+        array_push( $rows, $event );
+}        
+
+$testsmod = $moduleFactory->getModule("_standard/tests");
+$tests=$testsmod->extend_date();
+if( $tests != "" )
+{
+    foreach ( $tests as $event )
+        array_push( $rows, $event );
+}
+
+foreach( $rows as $row )
 {
     $parts = explode( " ", $row["start"] );
     $parts1 = explode(" ", $row["end"] );
