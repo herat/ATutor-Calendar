86ce7317877bb59c1a0c655e0baf23ac33affd7f
Herat <herat@Herats-iMac.local>
2012-07-28 11:54:07 +0530

    google calendar class included in other files

--- google_calendar_update.php
+++ google_calendar_update.php
@@ -1,31 +1,27 @@
 <?php
-    define('AT_INCLUDE_PATH', '../../include/');
+    /****************************************************************/
+    /* ATutor Calendar Module                                       */
+    /* https://atutorcalendar.wordpress.com/                        */
+    /*                                                              */
+    /* This module provides standard calendar features in ATutor.   */
+    /*                                                              */
+    /* Author: Anurup Raveendran, Herat Gandhi                      */
+    /* This program is free software. You can redistribute it and/or*/
+    /* modify it under the terms of the GNU General Public License  */
+    /* as published by the Free Software Foundation.                */
+    /****************************************************************/
+
+	require_once 'includes/classes/googlecalendar.class.php';
+	define('AT_INCLUDE_PATH', '../../include/');
     require (AT_INCLUDE_PATH.'vitals.inc.php');
-    require_once 'Zend/Loader.php';
 
-    Zend_Loader::loadClass('Zend_Gdata');
-    Zend_Loader::loadClass('Zend_Gdata_AuthSub');
-    Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
-    Zend_Loader::loadClass('Zend_Gdata_HttpClient');
-    Zend_Loader::loadClass('Zend_Gdata_Calendar');
+	$gcalobj = new GoogleCalendar();
 
-    function getAuthSubHttpClient()
-    {
-        global $_SESSION, $_GET, $_authSubKeyFile, $_authSubKeyFilePassphrase;
-        $client = new Zend_Gdata_HttpClient();
-        if ($_authSubKeyFile != null) {
-            // set the AuthSub key
-            $client->setAuthSubPrivateKeyFile($_authSubKeyFile, $_authSubKeyFilePassphrase, true);
-        }
-        $client->setAuthSubToken($_SESSION['sessionToken']);
-        return $client;
-    }
-
-    $client = getAuthSubHttpClient();
+    $client = $gcalobj->getAuthSubHttpClient();
     $gdataCal = new Zend_Gdata_Calendar($client);
 
     $eventURL = $_GET['id'];
-    $command = $_GET["cmd"];
+    $command = $_GET['cmd'];
 
     try 
     {
--- json-events-gcal.php
+++ json-events-gcal.php
@@ -14,113 +14,35 @@
     /**
      * This file returns events from Google Calendar in JSON format.
      */
-    define('AT_INCLUDE_PATH', '../../include/');
+	require_once 'includes/classes/googlecalendar.class.php';
+	define('AT_INCLUDE_PATH', '../../include/');
     require (AT_INCLUDE_PATH.'vitals.inc.php');
-    require_once 'Zend/Loader.php';
 
-    Zend_Loader::loadClass('Zend_Gdata');
-    Zend_Loader::loadClass('Zend_Gdata_AuthSub');
-    Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
-    Zend_Loader::loadClass('Zend_Gdata_HttpClient');
-    Zend_Loader::loadClass('Zend_Gdata_Calendar');
-
-    $_authSubKeyFile = null; // Example value for secure use: 'mykey.pem'
-    $_authSubKeyFilePassphrase = null;
-
-    /**
-     * Returns a HTTP client object with the appropriate headers for communicating
-     * with Google using AuthSub authentication.
-     *
-     * Uses the $_SESSION['sessionToken'] to store the AuthSub session token after
-     * it is obtained.  The single use token supplied in the URL when redirected
-     * after the user succesfully authenticated to Google is retrieved from the
-     * $_GET['token'] variable.
-     *
-     * @return Zend_Http_Client
-     */
-    function getAuthSubHttpClient() {
-        global $_SESSION, $_GET, $_authSubKeyFile, $_authSubKeyFilePassphrase;
-        $client = new Zend_Gdata_HttpClient();
-        if ($_authSubKeyFile != null) {
-            // set the AuthSub key
-            $client->setAuthSubPrivateKeyFile($_authSubKeyFile, $_authSubKeyFilePassphrase, true);
-        }
-        $client->setAuthSubToken($_SESSION['sessionToken']);
-        return $client;
-    }
-
-    /**
-     * Checks validity of a token. If token is valid then proceed ahead
-     * otherwise the user will be logged out. To check token a dummy
-     * call to function getCalendarListFeed is made. If there are some 
-     * problems then the token is not valid.
-     *
-     * @return void
-     */
-    function outputCalendarListCheck($client) {
-        $gdataCal = new Zend_Gdata_Calendar($client);
-        $calFeed = $gdataCal->getCalendarListFeed();
-    }
+	$gcalobj = new GoogleCalendar();
     
-    function isvalidtoken( $tokent ) {
-        try {
-            $client = getAuthSubHttpClient();
-            outputCalendarListCheck($client);
-            return true;
-        }
-        catch(Zend_Gdata_App_HttpException $e) {
-            global $db;
-            $qry = "DELETE FROM ".TABLE_PREFIX."calendar_google_sync WHERE userid='".$_SESSION['member_id']."'";
-            mysql_query($qry,$db);
-            logout();
-        }
-    }
+     global $db;
+     $qry = "SELECT * FROM ".TABLE_PREFIX."calendar_google_sync WHERE userid='".$_SESSION['member_id']."'";
+     $res = mysql_query($qry,$db);
+     if(mysql_num_rows($res) > 0) {
+         $row = mysql_fetch_assoc($res);
+         $_SESSION['sessionToken'] = $row['token'];
+         if($gcalobj->isvalidtoken($_SESSION['sessionToken'])) {
+             $client = $gcalobj->getAuthSubHttpClient();
+             $query = "SELECT * FROM ".TABLE_PREFIX."calendar_google_sync WHERE userid='".$_SESSION['member_id']."'";
+             $res = mysql_query($query,$db);
+             $rowval = mysql_fetch_assoc($res);
+             $prevval = $rowval['calids'];
 
-    /**
-     * Processes loading of this code through a web browser.
-     * @return void
-     */
-    function processPageLoad() {
-        global $db;
-        $qry = "SELECT * FROM ".TABLE_PREFIX."calendar_google_sync WHERE userid='".$_SESSION['member_id']."'";
-        $res = mysql_query($qry,$db);
-        if(mysql_num_rows($res) > 0) {
-            $row = mysql_fetch_assoc($res);
-            $_SESSION['sessionToken'] = $row['token'];
-            if(isvalidtoken($_SESSION['sessionToken'])) {
-                $client = getAuthSubHttpClient();
-                $query = "SELECT * FROM ".TABLE_PREFIX."calendar_google_sync WHERE userid='".$_SESSION['member_id']."'";
-                $res = mysql_query($query,$db);
-                $rowval = mysql_fetch_assoc($res);
-                $prevval = $rowval['calids'];
-
-                outputCalendarByDateRange($client,$_GET['start'],$_GET['end'],$prevval);
-            }
-        }
-    }
-
-    function outputCalendarList($client) {
-        $gdataCal = new Zend_Gdata_Calendar($client);
-        $calFeed = $gdataCal->getCalendarListFeed();
-        echo "<a href='http://localhost/tmpp/google-calendar-for-atutor/ui.php?logout=true' >Logout</a><br/>
-        <form method='post' action='' onsubmit='window.opener.location.reload(false);window.close();'>";
-        echo "<h1>" . $calFeed->title->text . "</h1>\n";
-        echo "<ul>\n";
-        foreach ($calFeed as $calendar) {
-            //echo "\t<li>" . $calendar->title->text . "</li>\n";
-            echo "\t<input type='radio' name ='calid' value='".$calendar->id->text."'/>".$calendar->title->text."<br/>";
-        }
-        echo "</ul>\n";
-        echo "<input type='submit' value='Submit' />";
-        echo "</form>";
-    }
+             outputCalendarByDateRange($client,$_GET['start'],$_GET['end'],$prevval,$gcalobj);
+         }
+     }
 
     /**
      * Iterate through all the Google Calendars and create a JSON encoded array of events.
      *
      * @return array of events in JSON format
      */
-    function outputCalendarByDateRange($client, $startDate='2007-05-01', $endDate='2007-08-01',$idsofcal) {
+    function outputCalendarByDateRange($client, $startDate='2007-05-01', $endDate='2007-08-01',$idsofcal,$gcalobj) {
         $gdataCal = new Zend_Gdata_Calendar($client);
         $rows = array();
 
@@ -191,26 +113,5 @@
         
         //Return the events in the JSON format.
         echo $str;    
-    }
-
-    /**
-     * If there are some discrepancies in the session or user
-     * wants not to connect his/her Google Calendars with ATutor
-     * then this function will securely log out the user.
-     *
-     * @return void
-     */
-    function logout() {
-        // Carefully construct this value to avoid application security problems.
-        $php_self = htmlentities(substr($_SERVER['PHP_SELF'], 0,
-                    strcspn($_SERVER['PHP_SELF'], "\n\r")), ENT_QUOTES);
-        //Revoke access for the stored token
-        Zend_Gdata_AuthSub::AuthSubRevokeToken($_SESSION['sessionToken']);
-        unset($_SESSION['sessionToken']);
-        //Close this popup window
-        echo "<script>window.location.reload(false);</script>";
-        exit();
-    }
-
-    processPageLoad();
+    }    
 ?>
