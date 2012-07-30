4659f061946c66dcdeae4112d820ffc7eaf4c5f9
Herat Gandhi <herat_000@yahoo.co.in>
2012-07-30 08:26:11 +0530

    format patch

--- patch/assignments/module_extend_date.php
+++ patch/assignments/module_extend_date.php
@@ -1,84 +1,79 @@
 <?php
-@session_start();
-if (!defined('AT_INCLUDE_PATH')) { exit; }
+    @session_start();
+    if (!defined('AT_INCLUDE_PATH')) { exit; }
 
-    /**
-     * Extending the assignment dates to make them accessible to Calendar Module
-     * @param    :    none | course_id(if this function is going to be used for multiple courses)
-     * @return    :    array (assignment dates)
-     * @author    :    Anurup Raveendran
-     */
+        /**
+         * Extending the assignment dates to make them accessible to Calendar Module
+         * @param    :    none | course_id(if this function is going to be used for multiple courses)
+         * @return    :    array (assignment dates)
+         * @author    :    Anurup Raveendran, Herat Gandhi
+         */
 
 
-function assignments_extend_date($course_id=null){
+    function assignments_extend_date($course_id=null) {
 
-    global $db;
-    $assignments = array();
-    
-    if($course_id==null) $course_id = $_SESSION['course_id'];
-    
-    // get course title
-    $sql = "SELECT title 
-            FROM ".TABLE_PREFIX."courses 
-            WHERE course_id = '".$course_id."'";
-            
-    $result = mysql_query($sql,$db) or die(mysql_error());
-    $row = mysql_fetch_assoc($result);
-    $course_title= $row['title'];
-    
-    $sql = "SELECT assignment_id,title,date_due,date_cutoff
-            FROM ".TABLE_PREFIX."assignments
-            WHERE course_id = '".$course_id."'";
+        global $db;
+        $assignments = array();
+        
+        if ($course_id == null) {
+            $course_id = $_SESSION['course_id'];
+        }
+        
+        // get course title
+        $sql = "SELECT title 
+                FROM ". TABLE_PREFIX . "courses 
+                WHERE course_id = '". $course_id . "'";
+                
+        $result       = mysql_query($sql,$db) or die(mysql_error());
+        $row          = mysql_fetch_assoc($result);
+        $course_title = $row['title'];
+        
+        $sql = "SELECT assignment_id,title,date_due,date_cutoff
+                FROM " . TABLE_PREFIX . "assignments
+                WHERE course_id = '" . $course_id . "'";
 
-    $result = mysql_query($sql,$db) or die(mysql_error());
-    $row_count  = mysql_num_rows($result);
+        $result     = mysql_query($sql,$db) or die(mysql_error());
+        $row_count  = mysql_num_rows($result);
 
-    if($row_count > 0){
-    $index=0;
-        while($row = mysql_fetch_assoc($result)){
-            /*if(  strpos( $row['date_due']."", '0000-00-00' ) !== false ||
-                strpos( $row['date_cutoff']."", '0000-00-00' ) !== false )
-            {
-                continue;
-            }
-            else
-            {*/
+        if ($row_count > 0) {
+            $index = 0;
+            while ($row = mysql_fetch_assoc($result)) {
+                
                 $assignment_id = $row['assignment_id'];
-                $unix_ts = strtotime($row['date_due']);
-                $time = date("h:i A",$unix_ts);
-                if( strpos( $row['date_due']."", '0000-00-00' ) === false )
-                {
-                    $assignments[$index] =  array(
-                                    "id"=>rand(5000,9000)."",
-                                    "title"=> "Due date of ".$row['title'],
-                                    "start"=>$row['date_due'],
-                                    "end"=>$row['date_due'],
-                                    "allDay"=>false,
-                                    "color"=>"yellow",
-                                    "textColor" => "black",
-                                    "editable"=>false
-                                    ) ;
+                $unix_ts       = strtotime($row['date_due']);
+                $time          = date('h:i A',$unix_ts);
+                
+                if (strpos( $row['date_due'] . '', '0000-00-00' ) === false) {
+                    $assignments[$index] = array(
+                                    "id"        => rand(5000,9000) . '',
+                                    "title"     => 'Due date of ' . $row['title'],
+                                    "start"     => $row['date_due'],
+                                    "end"       => $row['date_due'],
+                                    "allDay"    => false,
+                                    "color"     => 'yellow',
+                                    "textColor" => 'black',
+                                    "editable"  => false
+                                );
                                  
                     $unix_ts = strtotime($row['date_cutoff']);                  
-                    $time = date("h:i A",$unix_ts);
+                    $time    = date('h:i A',$unix_ts);
                     $index++;
                 }
-                if( strpos($row['date_cutoff']."", '0000-00-00' ) === false )
-                {
-                $assignments[$index] =  array(
-                                "id"=>rand(5000,9000)."",
-                                "title"=>"Cut off date of ".$row['title'],
-                                "start"=>$row['date_cutoff'],
-                                "end"=>$row['date_cutoff'],
-                                "allDay"=>false,
-                                "color"=>"red",
-                                "textColor" => "white",
-                                "editable"=>false 
-                                 ) ;            
+                if (strpos($row['date_cutoff'] . '', '0000-00-00' ) === false) {
+                    $assignments[$index] = array(
+                                "id"        => rand(5000,9000).'',
+                                "title"     => 'Cut off date of ' . $row['title'],
+                                "start"     => $row['date_cutoff'],
+                                "end"       => $row['date_cutoff'],
+                                "allDay"    => false,
+                                "color"     => 'red',
+                                "textColor" => 'white',
+                                "editable"  => false 
+                            );            
                     $index++;
                 }
             }
         }    
-    return $assignments;
-}
+        return $assignments;
+    }
 ?>
--- patch/tests/module_extend_date.php
+++ patch/tests/module_extend_date.php
@@ -1,85 +1,75 @@
 <?php 
-@session_start();
-if (!defined('AT_INCLUDE_PATH')) { exit; }
+    @session_start();
+    if (!defined('AT_INCLUDE_PATH')) { exit; }
 
-    /**
-     * Extending the test dates to make them accessible to Calendar Module
-     * @param    :    none | course_id(if this function is going to be used for multiple courses)
-     * @return    :    array (test dates)
-     * @author    :    Anurup Raveendran
-     */
+        /**
+         * Extending the test dates to make them accessible to Calendar Module
+         * @param    :    none | course_id(if this function is going to be used for multiple courses)
+         * @return    :    array (test dates)
+         * @author    :    Anurup Raveendran
+         */
 
 
-function tests_extend_date($course_id=null){
+    function tests_extend_date($course_id=null) {
 
-    global $db;
-    $tests = array();
-    
-    if($course_id==null) $course_id = $_SESSION['course_id'];
-    
-     // get course title
-    $sql = "SELECT title 
-            FROM ".TABLE_PREFIX."courses 
-            WHERE course_id = '".$course_id."'";
-            
-    $result = mysql_query($sql,$db) or die(mysql_error());
-    $row = mysql_fetch_assoc($result);
-    $course_title= $row['title'];
-    
-    $sql = "SELECT title,test_id,start_date,end_date
-            FROM ".TABLE_PREFIX."tests
-            WHERE course_id = '".$course_id."'";
-
-    $result = mysql_query($sql,$db) or die(mysql_error());
-    $row_count  = mysql_num_rows($result);
-    
-
-    if($row_count > 0){
-        $index=0;
-        while($row = mysql_fetch_assoc($result)){
-            /*if(  strpos( $row['start_date']."", '0000-00-00' ) !== false ||
-                strpos( $row['end_date']."", '0000-00-00' ) !== false )
-            {
-                continue;
-            }
-            else
-            {*/
-            if(  strpos( $row['start_date']."", '0000-00-00' ) === false )
-            {
-                $unix_ts = strtotime($row['start_date']);
-                $time = date("h:i A",$unix_ts);
-                $tests[$index] = array(
-                            "id"=>rand(20000,25000)."",
-                            "title"=> "Start date of ".$row['title']." test",
-                            "start"=>$row['start_date'],
-                            "end"=>$row['start_date'],
-                            "allDay"=>false,
-                            "color"=>"lime",
-                            "textColor" => "black",
-                            "editable"=>false                        
-                             ) ;
+        global $db;
+        $tests = array();
         
-        $unix_ts = strtotime($row['end_date']);        
-        $time = date("h:i A",$unix_ts);
-                
-                $index++;
+        if ($course_id == null) {
+            $course_id = $_SESSION['course_id'];
         }
-        if( strpos( $row['end_date']."", '0000-00-00' ) === false )
-            {        
-                $tests[$index] = array(
-                            "id"=>rand(20000,25000)."",
-                            "title"=> "End date of ".$row['title']." test",
-                            "start"=>$row['end_date'],
-                            "end"=>$row['end_date'],
-                            "allDay"=>false,
-                            "color"=>"purple",
-                            "textColor" => "white",
-                            "editable"=>false                          
-                            ) ;
-            $index++;
+        
+         // get course title
+        $sql = "SELECT title 
+                FROM " . TABLE_PREFIX . "courses 
+                WHERE course_id = '" . $course_id . "'";
+                
+        $result       = mysql_query($sql,$db) or die(mysql_error());
+        $row          = mysql_fetch_assoc($result);
+        $course_title = $row['title'];
+        
+        $sql = "SELECT title,test_id,start_date,end_date
+                FROM " . TABLE_PREFIX . "tests
+                WHERE course_id = '" . $course_id . "'";
+
+        $result    = mysql_query($sql,$db) or die(mysql_error());
+        $row_count = mysql_num_rows($result);
+        
+        if ($row_count > 0) {
+            $index = 0;
+            while ($row = mysql_fetch_assoc($result)) {
+                if (strpos( $row['start_date'] . '', '0000-00-00' ) === false) {
+                    $unix_ts = strtotime($row['start_date']);
+                    $time    = date('h:i A',$unix_ts);
+                    $tests[$index] = array(
+                                "id"        => rand(20000,25000).'',
+                                "title"     => 'Start date of ' . $row['title']. ' test',
+                                "start"     => $row['start_date'],
+                                "end"       => $row['start_date'],
+                                "allDay"    => false,
+                                "color"     => 'lime',
+                                "textColor" => 'black',
+                                "editable"=>false                        
+                            );            
+                    $unix_ts = strtotime($row['end_date']);        
+                    $time    = date('h:i A',$unix_ts);
+                    $index++;
+                }
+                if (strpos( $row['end_date'] . '', '0000-00-00' ) === false) {        
+                    $tests[$index] = array(
+                                "id"        => rand(20000,25000) . '',
+                                "title"     => 'End date of ' . $row['title'] . ' test',
+                                "start"     => $row['end_date'],
+                                "end"       => $row['end_date'],
+                                "allDay"    => false,
+                                "color"     => 'purple',
+                                "textColor" => 'white',
+                                "editable"  => false                          
+                            );
+                    $index++;
+                }
             }
         }
+        return $tests;
     }
-    return $tests;
-}
 ?>
